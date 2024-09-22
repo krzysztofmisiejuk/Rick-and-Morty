fadf263896d22fda1403ba0a506f2efc790fe395
Krzysiek <kmisiejuk15@gmail.com>
2024-09-22 12:49:55 +0200

    updated functions and variables names

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const inputs = document.querySelectorAll('input[name=status]');
 const filterInput = document.getElementById('filter-by-name');
 let currentPage = 1;
 
-const dispalyImage = (parent, src) => {
+const displayImage = (parent, src) => {
 	const img = document.createElement('img');
 	img.setAttribute('src', `${src}`);
 	parent.appendChild(img);
@@ -17,13 +17,13 @@ const filterByName = () => {
 };
 
 const findStatus = () => {
-	let charakterStatus = 'alive';
+	let characterStatus = 'alive';
 	inputs.forEach((input) => {
 		if (input.checked) {
-			charakterStatus = input.value;
+			characterStatus = input.value;
 		}
 	});
-	return charakterStatus;
+	return characterStatus;
 };
 
 const showNoResultInfo = () => {
@@ -34,7 +34,7 @@ const showNoResultInfo = () => {
 	cardsContainer.appendChild(noResultInfo);
 };
 
-const assignCharacterInfo = (parent, name, status, species) => {
+const assignParamsToCharacter = (parent, name, status, species) => {
 	const charName = document.createElement('p');
 	const charStatus = document.createElement('p');
 	const charSpecies = document.createElement('p');
@@ -44,22 +44,28 @@ const assignCharacterInfo = (parent, name, status, species) => {
 	parent.append(charName, charStatus, charSpecies);
 };
 
-const createCard = async (currentPage = 1) => {
+const createCard = (image, name, status, species) => {
+	const card = document.createElement('div');
+	card.classList.add('card');
+	displayImage(card, image);
+	assignParamsToCharacter(card, name, status, species);
+	cardsContainer.append(card);
+};
+
+const updateCards = async (currentPage = 1) => {
 	try {
 		const characterName = filterByName();
 		const characterStatus = findStatus();
 
-		const response = await fetch(`https://rickandmortyapi.com/api/character?page=${currentPage}&status=${characterStatus}&name=${characterName}`);
+		const response = await fetch(
+			`https://rickandmortyapi.com/api/character?page=${currentPage}&status=${characterStatus}&name=${characterName}`
+		);
 		const data = await response.json();
 		cardsContainer.innerHTML = '';
 
 		if (data.results && data.results.length > 0) {
 			data.results.forEach(({ image, name, status, species }) => {
-				const card = document.createElement('div');
-				card.classList.add('card');
-				dispalyImage(card, image);
-				assignCharacterInfo(card, name, status, species);
-				cardsContainer.append(card);
+				createCard(image, name, status, species);
 			});
 		} else {
 			showNoResultInfo();
@@ -78,19 +84,17 @@ const nextPage = async () => {
 	} catch (error) {
 		console.error('ERROR: ', error);
 	}
-	createCard(currentPage);
+	updateCards(currentPage);
 };
 const prevPage = () => {
 	if (currentPage > 1) currentPage--;
-	createCard(currentPage);
+	updateCards(currentPage);
 };
 
-filterInput.addEventListener('input', () => createCard(1));
+filterInput.addEventListener('input', () => updateCards(1));
 inputs.forEach((input) => {
-	input.addEventListener('change', () => createCard(1));
+	input.addEventListener('change', () => updateCards(1));
 });
 nextBtn.addEventListener('click', nextPage);
 prevBtn.addEventListener('click', prevPage);
-document.addEventListener('DOMContentLoaded', createCard);
-
-
+document.addEventListener('DOMContentLoaded', updateCards(1));
